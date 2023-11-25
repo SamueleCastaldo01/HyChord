8342484b5bd65f03eba67a71cb6e684602bafd4a
Samuele Castaldo <43854144+SamueleCastaldo01@users.noreply.github.com>
2023-11-25 01:42:48 +0100

    Add files via upload

---
+++ aggiunta/amareTe.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../../styles/global';
+
+export default function AmareTe ({ a, b, c, d, e, f, g, cDiesis, eBemolle, fDiesis, aBemolle, bBemolle, accordiStru }) {
+    return (
+    <>
+    <View style={globalStyles.containerCantico}>
+
+        <View style={globalStyles.righeCantico}>
+            <Text style={accordiStru!="Testo"? globalStyles.textCantico: globalStyles.textCanticoNo}><Text style={globalStyles.textCanticoColo}>1. </Text>La ragione della vita è amare Te,</Text>
+        </View>
+
+        <View style={globalStyles.righeCantico}>
+            <Text style={accordiStru!="Testo"? globalStyles.textCantico: globalStyles.textCanticoNo}>adorarti e servirti e lodarti o Dio,</Text>
+        </View>
+
+        <View style={globalStyles.righeCantico}>
+            <Text style={accordiStru!="Testo"? globalStyles.textCantico: globalStyles.textCanticoNo}>una cosa ho chiesto a te Signor, e quella</Text>
+        </View>
+
+        <View style={globalStyles.righeCantico}>
+            <Text style={accordiStru!="Testo"? globalStyles.textCantico: globalStyles.textCanticoNo}>cercherò, ch’io dimori nella casa tua per</Text>
+        </View>
+
+        <View style={globalStyles.righeCantico}>
+            <Text style={accordiStru!="Testo"? globalStyles.textCantico: globalStyles.textCanticoNo}>sempre;</Text>
+        </View>
+
+    </View>
+    
+    </>
+    )
+}
